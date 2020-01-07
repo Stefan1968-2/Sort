a9f20208fc811b7ed671a6fb9aefb41c51e8f141
vagrant <vagrant@homestead>
2020-01-07 15:27:21 +0000

    Bubble Sort with optimization

--- BubbleSort.php
+++ BubbleSort.php
@@ -1,13 +1,20 @@
 <?php
   function bubbleSort(&$array) {
     for ($index1 = count($array) - 1; $index1 > 0 ; --$index1) {
+      $update = false;
+      
       for ($index2 = 0; $index2 < $index1; ++$index2) {
         if ($array[$index2] > $array[$index2 + 1]) {
           $temp = $array[$index2];
           $array[$index2] = $array[$index2 + 1];
           $array[$index2 + 1] = $temp;
+          $update = true;
         }
       }
+
+      if ($update) {
+        break;
+      }      
     }
   }
 ?>
